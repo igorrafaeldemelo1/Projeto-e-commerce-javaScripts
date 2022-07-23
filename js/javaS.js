3096fecb04a17e91a824d2d0913fa7e3f76e3e78
igorrafaeldemelo1 <86748756+igorrafaeldemelo1@users.noreply.github.com>
2022-07-23 19:21:13 -0300

    first commit

---
+++ js/javaS.js
@@ -0,0 +1,40 @@
+
+
+function login(){
+  let login = document.querySelector('.login')
+  login.style.display = 'block';
+}
+
+function fechar01(){
+  let login = document.querySelector('.login')
+  login.style.display = 'none';
+}
+
+function  registro(){
+  let registro = document.querySelector('.registro')
+   registro.style.display = 'block';
+}
+
+function fechar(){
+  let registro = document.querySelector('.registro')
+   registro.style.display = 'none';
+}
+
+function  menu(){
+  let menu = document.querySelector('.menu-bar')
+   menu.style.display = 'flex';
+   
+}
+
+function closes(){
+  let closes = document.querySelector('.menu-bar')
+   closes.style.display = 'none';
+}
+
+
+
+
+
+
+
+
